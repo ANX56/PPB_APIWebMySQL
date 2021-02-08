3136a1af0ac2b12fba5914b79f8672b1cae86de2
ANX56 <athallahrizaldi@gmail.com>
2021-02-08 13:10:20 +0700

    init

---
+++ app/src/main/java/com/duaempat/ppb_apiwebmysql/data_list.java
@@ -0,0 +1,14 @@
+package com.duaempat.ppb_apiwebmysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_data_list);
+    }
+}
